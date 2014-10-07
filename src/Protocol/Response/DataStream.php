4541a23de5b4b0eeba7654e5e88066f57b357563
Adam Noon <adam.noon@gettyimages.com>
2014-10-06 21:52:04 -0600

    Correctly read decimal values

--- src/Protocol/Response/DataStream.php
+++ src/Protocol/Response/DataStream.php
@@ -219,9 +219,9 @@ class DataStream {
 	 */
 	public function readDecimal() {
 		$scale = $this->readInt();
-		$value = $this->readVarint();
-		$len = strlen($value);
-		return substr($value, 0, $len - $scale) . '.' . substr($value, $len - $scale);
+		$value = $this->readShort();
+
+		return $value / (pow(10, $scale));
 	}
 
 	/**
